7aabf9a1778f5093342fdb54cb2a5d8d1ad4b035
intothejourneys <intothejourneys@gmail.com>
2020-11-11 21:49:17 +0900

    🚀 #5#26 / "LogOut, App" / [In-Progress]

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,84 @@
 import React, { useState } from 'react';
 import { Switch, Route, Redirect, withRouter } from "react-router-dom";
+import axios from "axios";
 
 import './App.css';
 import Login from './Login';
-import LogOut from './LogOut';
-import Signup from './Signup';
+import SignUp from './Signup';
+import MyPage from './MyPage';
+import ContentsPage from './ContentsPage';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userInfo, setUserInfo] = useState(null);
 
-  // 로그인 로그아웃에 각각 서버에서 받아오는 것, 상태 변경 후 리다이렉트 하는 것까지 넣어야 함
+  const LoginSuccess = () => {
+    axios
+      .get('http://34.64.248.85:8080/content')
+      .then(res => {
+        setIsLoggedIn(isLoggedIn)
+        setUserInfo(res.data)
+        this.props.history.push('/contentspage')
+      })
+  }
+
+  const LogOutHandler = () => {
+    axios
+      .post('http://34.64.248.85:8080/logout')
+      .then(() => {
+        setIsLoggedIn(!isLoggedIn)
+        this.props.history.push('/')
+      })
+  }
+
+  // MyPage와 ContentsPage에서 사용할 것
+  // if (isLoggedIn) {
+  //   return (
+  //       <div> 
+  //           <div className="logout">
+  //               <label>logout</label>
+  //               <button onClick={logOutHandler}>Log Out</button>
+  //           </div>
+  //       </div>
+  //   )
+  // }
 
   return (
     <div>
       <Switch>
         <Route 
           exact path="/"
-          render={() => { // 이렇게 쓰는 것이 맞는지?
+          render={() => {
             if (!isLoggedIn) { // isLoggedIn === false
               return <Redirect to='/login' />
             }
             return <Redirect to='/contentspage' />
           }}
-          />
-        <Route path="/login" component={Login} />
-        {/* <Route path="/logOut" component={LogOut} /> */}
+        />
+        <Route 
+          path="/login" 
+          render={() => 
+            <Login LoginSuccess={LoginSuccess} />
+          }
+        />
+        <Route 
+          path="/signup" 
+          render={() => 
+            <SignUp />
+          }
+        />
+        <Route 
+          path="/mypage" 
+          render={() =>
+            <MyPage userInfo={userInfo} LogOutHandler={LogOutHandler} />
+          }
+        />
+        <Route
+          path="/contentspage"
+          render={() =>
+            <ContentsPage userInfo={userInfo} LogOutHandler={LogOutHandler} />
+          }
+        />
       </Switch>
     </div>
   );
