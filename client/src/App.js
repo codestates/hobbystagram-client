8215081eace5bf77728daf1b0372df05fed296ab
newkidsean <68689478+newkidsean@users.noreply.github.com>
2020-11-11 17:30:47 +0900

    :zap: [Client]#3/LogIn setting/In-progress

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.css';
 import Login from './Login';
+import Signup from './Signup';
+
 
 function App() {
   return (
+    <div>
+
     <Login />
+    <br />
+    <Signup />
+      
+    </div>
   );
 }
 
